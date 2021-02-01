1c0f03a2397f7cd837ca41d7518abff8f3bcc1ff
Daniel Vigil <daniel.julian.vigil@gmail.com>
2021-02-01 17:49:29 -0500

    refactor

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
 const url = "https://api.quotable.io/random";
 
-function generateQuote() {
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+  console.log(data);
+  var html = "";
+  var quote = "";
+  var author = "";
+  quote = data.content;
+  author = data.author;
+
+  html +=
+    "<div class='quote'><i class='fa fa-quote-right' aria-hidden='true' id='q'></i> " +
+    quote +
+    "<i class='fa fa-quote-right'></i></div>";
+
+  html += "<div class='author'>" + author + "</div>";
+  
+  html += "<p><button type='light' class='btn btn-default'><a href='https://twitter.com/intent/tweet?text=" + quote + " -- " + author + " https://goo.gl/4Hecgs&hashtags=quotes' target='_blank' title='Tweet'><i class='fa fa-twitter' id='twitter'></i>Tweet</button></a></p>";
+  
+  $("#quote").html(html);
+});
+
+$("#quote-button").on("click", () => {
+  $("#quote").html("...loading...");
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
-      var html = "";
-      var quote = "";
-      var author = "";
-      quote = data.content;
-      author = data.author;
+    var html = "";
+    var quote = "";
+    var author = "";
+    quote = data.content;
+    author = data.author;
 
-      html +=
-        "<div class='quote'><i class='fa fa-quote-right' aria-hidden='true' id='q'></i> " +
-        quote +
-        "<i class='fa fa-quote-right'></i></div>";
+    html +=
+    "<div class='quote'><i class='fa fa-quote-right' aria-hidden='true' id='q'></i> " +
+    quote +
+    "<i class='fa fa-quote-right'></i></div>";
 
-      html += "<div class='author'>" + author + "</div>";
-      
-      html += "<p><button class='btn btn-default'><a href='https://twitter.com/intent/tweet?text=" + quote + " -- " + author + " https://goo.gl/4Hecgs&hashtags=quotes' target='_blank' title='Tweet'><i class='fa fa-twitter' id='twitter'></i>Tweet</button></a></p>";
-      
-      $("#quote").html(html);
-    });
-}
+  html += "<div class='author'>" + author + "</div>";
+
+    html += "<p><button type='light' class='btn btn-default'><a href='https://twitter.com/intent/tweet?text=" + quote + " -- " + author + " https://goo.gl/4Hecgs&hashtags=quotes' target='_blank' title='Tweet'><i class='fa fa-twitter' id='twitter'></i>Tweet</button></a></p>";
+
+    $("#quote").html(html);
+  });
+});
+
+
